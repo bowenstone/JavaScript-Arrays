d7ca31aaf7cee94cc634d784ad8c58931da7ff83
Matt Freebery <74575157+mfreebery@users.noreply.github.com>
2021-02-08 14:36:02 -0500

    added average, median, above 70 scores

--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,11 @@
 console.log("connected...")
 const numbers = [1,12,34,49,45,76,87,38,94,10,11,12,3,75,6,8,29,13,90,99];
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+/*for ( let j = 0; j < numbers.length; j++) {
+document.getElementById('output').innerHTML += `Median Score: ${numbers[j]}<br>`;
+}    */
+
 
 let numberOfStudents = numbers.length;
 for( let i = 0; i < numbers.length; i++){
@@ -23,3 +29,35 @@ let lowest = numbers[0];
 //}
 document.getElementById('output').innerHTML += `Lowest Score: ${lowest}<br>`;
 document.getElementById('output').innerHTML += `Highest Score: ${highest}<br>`;
+
+let total = 0
+for ( let j = 0; j < numbers.length; j++) {
+        total += numbers[j];
+    }
+let average = total / numbers.length;
+document.getElementById('output').innerHTML += `Average Score: ${average}<br>`;
+/*let temp = numbers
+for ( let i = 0; i < numbers.length; i++){
+    for ( let j = 0; j < numbers.length; j++) {
+        if ( numbers[i] < numbers[j] ){
+            temp [i] = numbers[j];
+        }
+    }
+    //document.getElementById('output').innerHTML += `Median Score: ${temp [i]}<br>`;
+}   */
+let median = 0 
+if ( numbers.length % 2 ){
+    median = numbers [(numbers.length / 2)]
+}
+else {
+    median = (numbers [numbers.length / 2 ]+ numbers [numbers.length / 2 - 1]) / 2
+}
+document.getElementById('output').innerHTML += `Median Score: ${median}<br>`;
+
+let above70 = [0]
+for ( let i = 0; i < numbers.length; i++ ) {
+    if (numbers[i] > 70) {
+        above70.push(numbers[i])
+    }
+}
+document.getElementById('output').innerHTML += `Number of Scores Above 70: ${above70.length}<br>`;
